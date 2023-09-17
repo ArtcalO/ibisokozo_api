7e3e02b447110785df1e1a8d8b08dd183a72ecd1
ArtcalO <niyca14@gmail.com>
2023-09-17 13:10:59 +0200

    add schema

--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Igisokozo(mnodels.Model):
 	id = models.BogAutoField(primary_key=True)
 	igisokozo = models.TextField()
+	itariki = models.DateTimeField(auto_now=True)
 
 	@property
 	def inyishu(self):
@@ -15,4 +16,6 @@ class InyishuIgisokozo(mnodels.Model):
 	id = models.BogAutoField(primary_key=True)
 	igisokozo = models.ForeignKey(Igisokozo, on_delete=models.CASCADE)
 	inyishu = models.TextField()
+	insiguro = models.TextField(default="...")
+	itariki = models.DateTimeField(auto_now=True)
 
--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -1,6 +1,26 @@
 import graphene
+from graphene_django import DjangoObjectType
+
+from api.models import Igisokozo, InyishuIgisokozo
+
+class IgisokozoType(DjangoObjectType):
+    class Meta:
+        model = Igisokozo
+        fields = "__all__"
+class InyishuIgisokozoType(DjangoObjectType):
+    class Meta:
+        model = InyishuIgisokozo
+        fields = "__all__"
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    ibisokozo_vyose = graphene.List(IgisokozoType)
+    inyishu_zyose = graphene.List(InyishuIgisokozoType)
+
+    def resolve_ibisokozo_vyose(root, info):
+        # We can easily optimize query count in the resolve method
+        return Igisokozo.objects.all()
+
+    def resolve_category_by_name(root, info, name):
+        return InyishuIgisokozo.objects.select_related("igisokozo").all()
 
 schema = graphene.Schema(query=Query)
