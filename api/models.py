01f47b71f7ca2ea6f471b6d56fc3f26ffd06e28c
ArtcalO <niyca14@gmail.com>
2023-09-18 12:09:47 +0200

    add some models

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Igisokozo(mnodels.Model):
-	id = models.BogAutoField(primary_key=True)
+class Igisokozo(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	igisokozo = models.TextField()
 	itariki = models.DateTimeField(auto_now=True)
 
@@ -12,8 +12,8 @@ class Igisokozo(mnodels.Model):
 		return InyishuIgisokozo.objects.get(igisokozo=self.id).inyishu
 	
 
-class InyishuIgisokozo(mnodels.Model):
-	id = models.BogAutoField(primary_key=True)
+class InyishuIgisokozo(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	igisokozo = models.ForeignKey(Igisokozo, on_delete=models.CASCADE)
 	inyishu = models.TextField()
 	insiguro = models.TextField(default="...")
