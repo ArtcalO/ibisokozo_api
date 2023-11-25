4a6408085e14742f767d6ef8fe3e284c4f373d5a
dondelice257 <dondelicedushime@gmail.com>
2023-11-25 14:25:52 +0200

    implement login and signup

--- api/models.py
+++ api/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
 
 # Create your models here.
+
+
+
+
+class CustomUser(AbstractUser):
+    profile_photo = models.ImageField(max_length=500, blank=True)
+    score = models.IntegerField(default=0)
+    
+
+
+ 
 
 class Igisokozo(models.Model):
 	id = models.BigAutoField(primary_key=True)
--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,35 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+       
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    groups = serializers.SlugRelatedField(
+        many=True,
+        queryset=Group.objects.all(),
+        slug_field='name'
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password', 'groups']  # Add other fields as needed
+        extra_kwargs = {'password': {'write_only': True}}  # Ensure that the password field is write-only
+
+    def create(self, validated_data):
+        groups_data = validated_data.pop('groups', [])
+        user = CustomUser.objects.create_user(**validated_data)
+        
+        # Set groups using the set() method
+        user.groups.set(groups_data)
+        
+        return user
 class IgisokozoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Igisokozo
