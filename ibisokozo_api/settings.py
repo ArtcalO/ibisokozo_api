dbebeb99947d3dc5f388f6f2ee3d73cb2073b4dc
dondelice257 <dondelicedushime@gmail.com>
2024-01-09 09:42:24 +0200

    remove dotenv

--- ibisokozo_api/settings.py
+++ ibisokozo_api/settings.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()  # Load environment variables from a .env file if available
+# load_dotenv()  # Load environment variables from a .env file if available
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
