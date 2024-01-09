2c6382c79c5dea60cc23ba81fa2073d2989db369
dondelice257 <dondelicedushime@gmail.com>
2024-01-09 09:39:52 +0200

    switch back to sqlite from postgres

--- ibisokozo_api/settings.py
+++ ibisokozo_api/settings.py
@@ -64,12 +64,8 @@ WSGI_APPLICATION = 'ibisokozo_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ibisokozo',
-        'USER': 'dondelice257',
-        'PASSWORD': 'helloworld',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
