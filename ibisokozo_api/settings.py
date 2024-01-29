02b7c9f8d449828b504498ba88820cc5aabdcdbb
dondelice257 <dondelicedushime@gmail.com>
2024-01-29 17:48:53 +0200

    correct storage duplication

--- ibisokozo_api/settings.py
+++ ibisokozo_api/settings.py
@@ -133,9 +133,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
+    # "default": {
+    #     "BACKEND": "django.core.files.storage.FileSystemStorage",
+    # },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
